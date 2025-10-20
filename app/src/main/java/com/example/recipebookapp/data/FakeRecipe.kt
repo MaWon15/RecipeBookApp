7b09248fcc48ba9257fc07ef40994d3128501047
Diana Maldonado <diana.maldonado0407@gmail.com>
2025-10-20 15:49:23 -0700

    Added 50 fake recipes, place holders

--- app/src/main/java/com/example/recipebookapp/data/FakeRecipe.kt
+++ app/src/main/java/com/example/recipebookapp/data/FakeRecipe.kt
@@ -32,7 +32,340 @@ object FakeRecipe {
                 "Fold in blueberries.",
                 "Cook until bubbles form; flip and finish."
             )
-        )
+        ),
+        Recipe(
+            id = 4,
+            title = "Blueberry Pancakes",
+            ingredients = listOf("1 cup flour", "1 tbsp sugar", "1 tsp baking powder", "1 egg", "3/4 cup milk", "Blueberries"),
+            steps = listOf(
+                "Whisk dry ingredients, then add egg and milk.",
+                "Fold in blueberries.",
+                "Cook until bubbles form; flip and finish."
+            )
+        ),
+        Recipe(
+            id = 5,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 6,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 7,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 8,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 9,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 10,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 11,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 12,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 13,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 14,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 15,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 16,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 17,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 18,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 19,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 20,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 21,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 22,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 23,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 24,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 25,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 26,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 27,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 28,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 29,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 30,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 31,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 32,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 33,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 34,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 35,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 36,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 37,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 38,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 39,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 40,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 41,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 42,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 43,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 44,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 45,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 46,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 47,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 48,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 49,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+        Recipe(
+            id = 50,
+            title = "",
+            ingredients = listOf(""),
+            steps = listOf(
+            )
+        ),
+
     )
     //Sort By alphabet by name of Recipe
         .sortedBy { it.title }
