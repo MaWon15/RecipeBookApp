bc9d33b9728c10a73e8ea2f98bb4c2dcb5ebe4be
MaWon15 <stevedang0105@gmail.com>
2025-10-20 09:55:35 -0700

    Generate

--- app/src/main/java/com/example/recipebookapp/vm/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipebookapp/vm/RecipeViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.recipebookapp.vm
 
-class RecipeViewModel {
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import com.example.recipebookapp.data.FakeRecipe
+import com.example.recipebookapp.data.Recipe
+
+
+class RecipeViewModel : ViewModel() {
+
+    //Taking all from FakeRecipe.kt
+    var recipes by mutableStateOf(FakeRecipe.all)
+        private set
+
+    //Storing favorites recipe IDs
+    var favoriteIds by mutableStateOf(setOf<Int>())
+        private set
+
+    //ToggleFavorite List
+
+    fun toggleFavorite(id: Int){
+        favoriteIds = if (id in favoriteIds) favoriteIds - id else favoriteIds + id
+    }
+
+    fun isFavorite(id : Int): Boolean = id in favoriteIds
+
+    fun getRecipe(id: Int): Recipe? = recipes.find {it.id == id }
+
 }
